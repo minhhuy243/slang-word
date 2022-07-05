34bb2e6b9dab5a17d8acb7630398741cdc8bdfb4
minhhuy243 <minhhuy243@gmail.com>
2022-07-05 22:21:58 +0700

    update UI & add feature read file

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -17,17 +17,43 @@ import javax.swing.SwingConstants;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map.Entry;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
 
-public class MainFrame extends JFrame {
-	private JTextField txtAsd;
-	private JTextField textField;
-	private JTextField textField_1;
 
+public class MainFrame extends JFrame {
+	private JTextField txtKeyword;
+	private JTextField txtSlang;
+	private JTextField txtDefinition;
+	private JButton btnHistory;
+	private JButton btnSearch;
+	private JComboBox cbSearchType;
+	private JLabel lblRandom;
+	private JButton btnRandom;
+	private JLabel lblKeyword;
+	private JLabel lblSlang;
+	private JLabel lblDefinition;
+	private JButton btnAdd;
+	private JButton btnEdit;
+	private JLabel lblQuiz;
+	private JLabel lblQuizMode;
+	private JComboBox cbQuizMode;
+	
+	private String[] columnNames = new String [] {
+            "No.", "Slang", "Definition"};
+
+	private SlangWord slangWord;
+	private JTable tbSlangWord;
+	private JButton btnRestoreDefault;
+	private JButton btnDelete;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -46,120 +72,201 @@ public class MainFrame extends JFrame {
 
 	/**
 	 * Create the application.
+	 * @throws IOException 
 	 */
-	public MainFrame() {
+	public MainFrame() throws IOException {
+		initialize();
+		slangWord = SlangWord.getInstance();
+		display();
+	}
+
+	public void initialize() {
 		getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 19));
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(724, 115, 502, 515);
+		scrollPane.setBounds(724, 115, 502, 557);
 		
-		JLabel lblNewLabel = new JLabel("abc");
-		lblNewLabel.setBounds(262, 10, 545, 41);
+		JLabel lblNewLabel = new JLabel("Slang Words Dictionary");
+		lblNewLabel.setBounds(0, 10, 1236, 41);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		
-		JLabel lblNewLabel_2 = new JLabel("Keyword:");
-		lblNewLabel_2.setBounds(29, 116, 98, 23);
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblKeyword = new JLabel("Keyword:");
+		lblKeyword.setBounds(29, 116, 98, 23);
+		lblKeyword.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JLabel lblNewLabel_2_1 = new JLabel("abc: ...................................");
-		lblNewLabel_2_1.setBounds(272, 61, 508, 31);
-		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblRandom = new JLabel("#!@#: ...................................");
+		lblRandom.setBounds(337, 61, 508, 31);
+		lblRandom.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRandom.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(801, 61, 131, 31);
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnRandom = new JButton("Random");
+		btnRandom.setBounds(855, 61, 131, 31);
+		btnRandom.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(527, 115, 141, 31);
-		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		cbSearchType = new JComboBox();
+		cbSearchType.setBounds(527, 115, 141, 31);
+		cbSearchType.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JButton btnNewButton_1 = new JButton("Search");
-		btnNewButton_1.setBounds(137, 166, 131, 31);
-		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnSearch = new JButton("Search");
+		btnSearch.setBounds(137, 166, 131, 31);
+		btnSearch.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JButton btnNewButton_2 = new JButton("History");
-		btnNewButton_2.setBounds(306, 166, 131, 31);
-		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnHistory = new JButton("History");
+		btnHistory.setBounds(306, 166, 131, 31);
+		btnHistory.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JLabel lblNewLabel_2_2 = new JLabel("Keyword:");
-		lblNewLabel_2_2.setBounds(29, 236, 98, 23);
-		lblNewLabel_2_2.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblSlang = new JLabel("Slang:");
+		lblSlang.setBounds(29, 236, 98, 23);
+		lblSlang.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		txtAsd = new JTextField();
-		txtAsd.setBounds(137, 116, 368, 29);
-		txtAsd.setText("asd");
-		txtAsd.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		txtAsd.setColumns(10);
+		txtKeyword = new JTextField();
+		txtKeyword.setBounds(137, 116, 368, 29);
+		txtKeyword.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		txtKeyword.setColumns(10);
 		
-		JLabel lblNewLabel_2_2_1 = new JLabel("Keyword:");
-		lblNewLabel_2_2_1.setBounds(29, 280, 98, 23);
-		lblNewLabel_2_2_1.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblDefinition = new JLabel("Definition:");
+		lblDefinition.setBounds(29, 280, 98, 23);
+		lblDefinition.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JButton btnNewButton_1_1 = new JButton("Search");
-		btnNewButton_1_1.setBounds(137, 331, 131, 31);
-		btnNewButton_1_1.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnAdd = new JButton("Add");
+		btnAdd.setBounds(137, 331, 131, 31);
+		btnAdd.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JButton btnNewButton_1_2 = new JButton("Search");
-		btnNewButton_1_2.setBounds(443, 331, 131, 31);
-		btnNewButton_1_2.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnDelete = new JButton("Delete");
+		btnDelete.setBounds(443, 331, 131, 31);
+		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		JButton btnNewButton_1_3 = new JButton("Search");
-		btnNewButton_1_3.setBounds(286, 331, 131, 31);
-		btnNewButton_1_3.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnEdit = new JButton("Edit");
+		btnEdit.setBounds(286, 331, 131, 31);
+		btnEdit.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		
-		textField = new JTextField();
-		textField.setBounds(137, 233, 531, 29);
-		textField.setText("asd");
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		textField.setColumns(10);
+		txtSlang = new JTextField();
+		txtSlang.setBounds(137, 233, 531, 29);
+		txtSlang.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		txtSlang.setColumns(10);	
 		
-		JTable table = new JTable();
-		table.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"1", "AAA", null},
-				{"2", "22", null},
-			},
-			new String[] {
-				"New column", "New column", "New column"
-			}
-		));
-		scrollPane.setViewportView(table);
+		tbSlangWord = new JTable();
+		tbSlangWord.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		scrollPane.setViewportView(tbSlangWord);
 		getContentPane().setLayout(null);
-		getContentPane().add(btnNewButton_2);
-		getContentPane().add(comboBox);
-		getContentPane().add(lblNewLabel_2_1);
-		getContentPane().add(btnNewButton_1_1);
-		getContentPane().add(btnNewButton_1_2);
-		getContentPane().add(btnNewButton_1_3);
-		getContentPane().add(lblNewLabel_2_2_1);
-		getContentPane().add(textField);
-		getContentPane().add(lblNewLabel_2);
-		getContentPane().add(txtAsd);
-		getContentPane().add(lblNewLabel_2_2);
-		getContentPane().add(btnNewButton_1);
+		getContentPane().add(btnHistory);
+		getContentPane().add(cbSearchType);
+		getContentPane().add(lblRandom);
+		getContentPane().add(btnAdd);
+		getContentPane().add(btnDelete);
+		getContentPane().add(btnEdit);
+		getContentPane().add(lblDefinition);
+		getContentPane().add(txtSlang);
+		getContentPane().add(lblKeyword);
+		getContentPane().add(txtKeyword);
+		getContentPane().add(lblSlang);
+		getContentPane().add(btnSearch);
 		getContentPane().add(scrollPane);
 		getContentPane().add(lblNewLabel);
-		getContentPane().add(btnNewButton);
+		getContentPane().add(btnRandom);
 		
-		textField_1 = new JTextField();
-		textField_1.setText("asd");
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		textField_1.setColumns(10);
-		textField_1.setBounds(137, 277, 531, 29);
-		getContentPane().add(textField_1);
+		txtDefinition = new JTextField();
+		txtDefinition.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		txtDefinition.setColumns(10);
+		txtDefinition.setBounds(137, 277, 531, 29);
+		getContentPane().add(txtDefinition);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(29, 372, 639, 331);
+		panel.setBounds(29, 372, 639, 371);
 		getContentPane().add(panel);
+		panel.setLayout(null);
 		
-		JButton btnNewButton_1_2_1 = new JButton("Search");
-		btnNewButton_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		btnNewButton_1_2_1.setBounds(908, 653, 131, 31);
-		getContentPane().add(btnNewButton_1_2_1);
-		this.setBounds(100, 100, 1250, 750);
+		lblQuiz = new JLabel("Quiz Game");
+		lblQuiz.setHorizontalAlignment(SwingConstants.CENTER);
+		lblQuiz.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblQuiz.setBounds(0, 10, 639, 23);
+		panel.add(lblQuiz);
+		
+		lblQuizMode = new JLabel("Mode:");
+		lblQuizMode.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblQuizMode.setBounds(10, 47, 98, 23);
+		panel.add(lblQuizMode);
+		
+		cbQuizMode = new JComboBox();
+		cbQuizMode.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		cbQuizMode.setBounds(111, 43, 141, 31);
+		panel.add(cbQuizMode);
+		
+		JLabel lblQuestionTitle = new JLabel("Question:");
+		lblQuestionTitle.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblQuestionTitle.setBounds(10, 101, 98, 23);
+		panel.add(lblQuestionTitle);
+		
+		JLabel lblQuizQuestion = new JLabel("");
+		lblQuizQuestion.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblQuizQuestion.setBounds(111, 101, 528, 23);
+		panel.add(lblQuizQuestion);
+		
+		JButton btnQuizAnswerA = new JButton("A.");
+		btnQuizAnswerA.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnQuizAnswerA.setBounds(10, 149, 290, 56);
+		panel.add(btnQuizAnswerA);
+		
+		JButton btnQuizAnswerB = new JButton("B.");
+		btnQuizAnswerB.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnQuizAnswerB.setBounds(339, 149, 290, 56);
+		panel.add(btnQuizAnswerB);
+		
+		JButton btnQuizAnswerC = new JButton("C.");
+		btnQuizAnswerC.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnQuizAnswerC.setBounds(10, 241, 290, 56);
+		panel.add(btnQuizAnswerC);
+		
+		JButton btnQuizAnswerD = new JButton("D.");
+		btnQuizAnswerD.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnQuizAnswerD.setBounds(339, 241, 290, 56);
+		panel.add(btnQuizAnswerD);
+		
+		JButton btnQuizStart = new JButton("Start");
+		btnQuizStart.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnQuizStart.setBounds(64, 319, 131, 31);
+		panel.add(btnQuizStart);
+		
+		JButton btnQuizNextQuestion = new JButton("Next Question");
+		btnQuizNextQuestion.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnQuizNextQuestion.setBounds(240, 319, 153, 31);
+		panel.add(btnQuizNextQuestion);
+		
+		JButton btnQuizStop = new JButton("Stop");
+		btnQuizStop.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnQuizStop.setBounds(437, 319, 131, 31);
+		panel.add(btnQuizStop);
+		
+		btnRestoreDefault = new JButton("Restore Default");
+		btnRestoreDefault.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		btnRestoreDefault.setBounds(871, 691, 197, 31);
+		getContentPane().add(btnRestoreDefault);
+		this.setBounds(100, 100, 1250, 800);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		initialize();
 	}
+	
+	public void display() throws IOException {
+		if (slangWord.readFile(FileNameUtils.DEFAULT)) {
+			DefaultTableModel model = new DefaultTableModel(null, columnNames);
+			int i = 0;
+			Object[] data;
+			for (Entry<String, List<String>> entry : slangWord.getData().entrySet()) {
+				List<String> definitions = entry.getValue();
+				for(String definition : definitions) {
+					data = new Object[] {
+							++i, entry.getKey(), definition 
+					};
+					model.addRow(data);
+				}
+			}
+			tbSlangWord.setModel(model);
+		}
+	
+		tbSlangWord.getColumnModel().getColumn(0).setMaxWidth(50);
+		btnAdd.setEnabled(true);
+		btnEdit.setEnabled(false);
+		btnDelete.setEnabled(false);
+	}
+	
 }
--- src/SlangWord.java
+++ src/SlangWord.java
@@ -1,17 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 
+
 public class SlangWord {
 	
-	private Map<String, List<String>> slangWords = new HashMap();
+	private Map<String, List<String>> slangWords = new LinkedHashMap<>();
 	
-    private static final String DEFAULT_FILE_NAME = "slang.txt";
-    private static final String ORIGINAL_FILE_NAME = "original-slang.txt";
-    private static final String HISTORY_FILE_NAME = "history-slang.txt";
+//    private static final String DEFAULT_FILE_NAME = "slang.txt";
+//    private static final String ORIGINAL_FILE_NAME = "original-slang.txt";
+//    private static final String HISTORY_FILE_NAME = "history-slang.txt";
     private static final String NEW_LINE_SEPARATOR = "\n";
-	private static final String GRAVE_ACCENT_DELIMITER = "`";
-	private static final String PIPE_DELIMITER = "|";
+	private static final String GRAVE_ACCENT_DELIMITER = "\\`";
+	private static final String PIPE_DELIMITER = "\\|";
     
 	/*
 	 * Bill Pugh Singleton Implementation
@@ -28,5 +40,65 @@ public class SlangWord {
 	public static SlangWord getInstance() {
 		return SingletonHelper.INSTANCE;
 	}
+	
+	public Map<String, List<String>> getData() {
+		return slangWords;
+	}
+	
+//	public Boolean writeFile(String fileName) throws IOException {
+//        BufferedWriter bw = null;
+//        try {
+//            bw = new BufferedWriter(new FileWriter(fileName));
+//            for(Student student : students) {
+//                bw.append(student.getId().toString());
+//                bw.append(COMMA_DELIMITER);
+//                bw.append(student.getName());
+//                bw.append(COMMA_DELIMITER);
+//                bw.append(student.getScore() == null ? "" : student.getScore().toString());
+//                bw.append(COMMA_DELIMITER);
+//                bw.append(student.getImage());
+//                bw.append(COMMA_DELIMITER);
+//                bw.append(student.getAddress());
+//                bw.append(COMMA_DELIMITER);
+//                bw.append(student.getNote());
+//                bw.append(NEW_LINE_SEPARATOR);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return false;
+//        } finally {
+//            bw.flush();
+//            bw.close();
+//        }
+//        return true;
+//    }
+//	
+	
+	public Boolean readFile(String fileName) throws IOException {
+    	Path path = null;
+        BufferedReader br = null;
+        long startTime = System.currentTimeMillis();
+        try {
+        	path = Paths.get(fileName);
+            br = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+            slangWords.clear();
+            String csvLine;
+            
+            while ((csvLine = br.readLine()) != null) {
+            	String[] data = csvLine.split(GRAVE_ACCENT_DELIMITER, -1);
+                slangWords.put(data[0], Arrays.asList(data[1].split(PIPE_DELIMITER, -1)));
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            br.close();
+        }
+        
+		long endTime = System.currentTimeMillis();
+		long timeElapsed = endTime - startTime;
+		System.out.println("Execution time in milliseconds: " + timeElapsed);
+        return true;
+    }
 	
 }
